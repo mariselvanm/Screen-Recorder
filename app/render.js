4b8d21684988d718287e2629048a5efff13fc9be
Mariselvan <mariselvan.murugasan@zoomrx.com>
2020-12-18 19:21:42 +0530

    Functionaly working app

---
+++ app/render.js
@@ -0,0 +1,76 @@
+const { desktopCapturer, remote } = require('electron');
+
+const { writeFile } = require('fs');
+
+let recorder;
+let blobs = [];
+
+// Buttons
+const videoElement = document.querySelector('video');
+
+const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+  captureScreenVideoWithAudio();
+  startBtn.classList.add('is-danger');
+  startBtn.innerText = 'Recording';
+};
+
+const stopBtn = document.getElementById('stopBtn');
+stopBtn.onclick = e => {
+  recorder.stop();
+  startBtn.classList.remove('is-danger');
+  startBtn.innerText = 'Start';
+};
+
+function captureScreenVideoWithAudio() {
+  navigator.webkitGetUserMedia({
+    audio: true
+  }, function(audioStream) {
+    navigator.webkitGetUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'screen',
+          minWidth: 1280,
+          minHeight: 720
+        }
+      }
+    }, handleVideoStream(audioStream), handleUserMediaError);
+  }, function() {});
+}
+
+function handleVideoStream(audioStream) {
+  return function(videoStream) {
+    if (audioStream) {
+      let audioTracks = audioStream.getAudioTracks();
+      if (audioTracks.length > 0) {
+        videoStream.addTrack(audioTracks[0]);
+      }
+    }
+    videoElement.srcObject = videoStream;
+    videoElement.muted = true;
+    videoElement.play();
+    recorder = new MediaRecorder(videoStream);
+    blobs = [];
+    recorder.ondataavailable = function(event) {
+      blobs.push(event.data);
+    };
+    recorder.onstop =async function(event) {
+      const blob = new Blob(blobs, {
+        type: 'video/webm; codecs=vp9'
+      });
+    
+      const buffer = Buffer.from(await blob.arrayBuffer());
+
+      filePath ='/Users/zoomrx/Desktop/sample.webm';
+      if (filePath) {
+        writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+      }
+    }
+    recorder.start();
+  };
+}
+
+function handleUserMediaError(e) {
+  console.error('handleUserMediaError', e);
+}
--- preload.js
+++ preload.js
@@ -1,5 +1,26 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('content loaded');
+  const { desktopCapturer, remote } = require('electron');
+  const { Menu } = remote;
+
+  const videoSelectBtn = document.getElementById('videoSelectBtn');
+  videoSelectBtn.onclick = getVideoSources;
+
+  async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+      types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+      inputSources.map(source => {
+        return {
+          label: source.name,
+          click: () => selectSource(source)
+        };
+      })
+    );
+
+    videoOptionsMenu.popup();
+  }
 })
